a1e8409353148121473243386a808cfedf984ebf
LazMan8 <laza.andriamampianina67@outlook.com>
2024-09-27 14:16:11 +0200

    refais la page de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,138 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<section class="vh-100 gradient-custom">
+    <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                    <div class="card-body p-5 text-center">
+                        <div class="mb-md-5 mt-md-4 pb-5">
+
+                            <h2 class="fw-bold mb-2 text-uppercase">{{ __('Login') }}</h2>
+                            <p class="text-white-50 mb-5">Please enter your login and password!</p>
+
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+
+                                <!-- Partie mail -->
+                                <div class="form-outline form-white mb-4">
+                                    <label class="form-label" for="email">{{ __('Email Address') }}</label>
+                                    <input id="email" type="email" class="form-control form-control-lg @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                    @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+
+                                <!-- Partie mot de passe -->
+                                <div class="form-outline form-white mb-4">
+                                    <label class="form-label" for="password">{{ __('Password') }}</label>
+                                    <input id="password" type="password" class="form-control form-control-lg @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                    @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+
+                                <!-- Partie souvenir avec un bouton remember me -->
+                                <div class="row mt-5 justify-content-center">
+                                    <div class="col-md-4  ">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                            <label class="form-check-label" for="remember">
+                                                {{ __('Remember Me') }}
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <!-- Lien vers la page de récupération du mot de passe -->
+                                <div class="row mb-5">
+                                    <div class="col-md-6 offset-md-3 ">
+                                        @if (Route::has('password.request'))
+                                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                                            {{ __('Forgot Your Password?') }}
+                                        </a>
+                                        @endif
+                                    </div>
+                                </div>
+
+                                <!-- Bouton de soumission -->
+                                <button class="btn btn-outline-light btn-lg px-5 " type="submit">{{ __('Login') }}</button>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+{{-- <section class="vh-100 gradient-custom">
+    <div class="container py-5 h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+          <div class="card bg-dark text-white" style="border-radius: 1rem;">
+            <div class="card-body p-5 text-center">
+              <div class="mb-md-5 mt-md-4 pb-5">
+                <div>{{ __('Login') }}</div>
+                <form action="post" action="{{ route('login') }}">
+                @csrf
+                    <h2 class="fw-bold mb-2 text-uppercase">{{ __('Login') }}</h2>
+                    <p class="text-white-50 mb-5">Please enter your login and password!</p>
+
+                    <!-- Partie mail -->
+                    <div data-mdb-input-init class="form-outline form-white mb-4">
+                        <label class="form-label" for="email">{{ __('Email Address') }}</label>
+                        <input id="email" type="email" class="form-control form-control-lg @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus/>
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+
+                    <!-- Partie mot de passe -->
+                    <div data-mdb-input-init class="form-outline form-white mb-4">
+                        <label class="form-label" for="password">{{ __('Password') }}</label>
+                        <input id="password" type="password" class="form-control form-control-lg @error('password') is-invalid @enderror" name="password" required autocomplete="current-password"/>
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                    </div>
+
+                    <!-- Partie souvenir avec un bouton remeber me-->
+                    <div class="row mb-4">
+                        <div class="col-md-6 offset-md-3 ">
+                            <input class="form-check-input mb-4" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                            <label class="form-check-label" for="remember">
+                                {{ __('Remember Me') }}
+                            </label>
+                            <!-- Lien vers la page de récupération du mot de passe -->
+                            @if (Route::has('password.request'))
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
+                               @endif
+                        </div>
+                    </div>
+                    <button data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-light btn-lg px-5" type="submit">{{ __('Login') }}</button>
+                </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section> --}}
+
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -69,5 +200,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 @endsection
