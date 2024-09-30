6bddadd3eb0896b1c04ed35256fa794114b8f745
LazMan8 <laza.andriamampianina67@outlook.com>
2024-09-30 15:50:04 +0200

    dashboard et bouton edit fonctioonnel

--- resources/views/editUser.blade.php
+++ resources/views/editUser.blade.php
@@ -1,11 +1,78 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+
+<section class="vh-100 gradient-custom">
+    <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                    <div class="card-body p-5 text-center">
+                        <div class="mb-md-3 mt-md-4">
+
+                            <h2 class="fw-bold mb-2 text-uppercase">{{ __('Mofication user : ') }}</h2>
+                            <p class="text-white-50 mb-5">Veuillez remplir les champs pour modifier.</p>
+
+                            <form method="POST" action="{{ route('update') }}">
+                            @csrf
+
+                                <!-- Partie nom -->
+                                <div class="form-outline form-white mb-4">
+                                    <label class="form-label" for="name">{{ __('Name') }} : </label>
+                                    <input id="name" type="text" class="form-control form-control-lg @error('name') is-invalid @enderror" name="name" value="{{ old('email', $user->name) }}" required autocomplete="name" autofocus>
+                                    @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+
+                                <!-- Partie mail -->
+                                <div class="form-outline form-white mb-4">
+                                    <label class="form-label" for="email">{{ __('Email Address') }} :</label>
+                                    <input id="email" type="email" class="form-control form-control-lg @error('email') is-invalid @enderror" name="email" value="{{ old('email', $user->email) }}" required autocomplete="email" autofocus>
+                                    @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+
+                                <!-- Partie creation de mot de passe -->
+                                <div class="form-outline form-white mb-4">
+                                    <label class="form-label" for="password">{{ __('Password') }} : </label>
+                                    <input id="password" type="password" class="form-control form-control-lg @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                    @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+
+                                <!-- Partie confirmation du mot de passe -->
+                                <div class="form-outline form-white mb-5">
+                                    <label class="form-label" for="password-confirm">{{ __('Confirm Password') }} : </label>
+                                    <input id="password-confirm" type="password" class="form-control form-control-lg" name="password_confirmation" required autocomplete="new-password">
+                                </div>
+
+                                <!-- Bouton de soumission -->
+                                <button class="btn btn-outline-light btn-lg px-5 mt-4" type="submit">{{ __('Modifier') }}</button>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Mofication') }}</div>
+                <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
@@ -73,5 +140,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 @endsection
